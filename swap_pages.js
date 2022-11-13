0919e239ccc299704c7eab357f8664ba092022e9
fergus-t <62132478+fergus-t@users.noreply.github.com>
2022-11-13 22:06:55 +0800

    Add files via upload

---
+++ swap_pages.js
@@ -0,0 +1,15 @@
+
+function organizepages() {
+    var numofPages = this.numPages;
+    var numofOddPages = Math.floor(numofPages/2);
+
+    //go to the first page.
+    this.pageNum = 0; 
+
+    for(i = 0; i < numofOddPages; i++) {
+        this.movePage(numofPages-1, i*2);
+    }
+    app.alert("Page order swapped!");
+    return "Success";
+}
+organizepages();
